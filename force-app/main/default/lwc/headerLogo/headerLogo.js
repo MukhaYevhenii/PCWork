f4e97dbe9d0cd5a0c4f9e9d219e5f6217d5f72c0
yevhenii.mukha <yevhenii.mukha@britenet.com.pl>
2022-06-12 20:15:01 +0200

    Add site experience

--- force-app/main/default/lwc/headerLogo/headerLogo.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import pcworkLogo from '@salesforce/resourceUrl/PCworklogo';
-
-export default class HeaderLogo extends LightningElement {
-    logo = pcworkLogo;
-}
